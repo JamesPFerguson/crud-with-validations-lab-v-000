a8ee3755ef1c7662844311298ac6f4268757a114
James Ferguson <ncjamesferguson@gmail.com>
2018-03-25 22:06:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,18 @@
 class SongsController < ApplicationController
 
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -24,5 +33,5 @@ class SongsController < ApplicationController
   def song_params
     params.permit(:title, :released, :release_year, :artist_name, :genre)
   end
-  
+
 end
