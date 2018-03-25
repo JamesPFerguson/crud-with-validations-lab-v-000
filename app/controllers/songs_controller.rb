214d3f9b9a62cd50ac3a0aaad79fa5b357bf7a32
James Ferguson <ncjamesferguson@gmail.com>
2018-03-25 15:30:35 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,25 @@
+class SongsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+
+end
